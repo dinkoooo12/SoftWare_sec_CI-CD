41d40412ebe240d9370aff51b6566bc1882aabdd
Dinka <naafefa123@gmail.com>
2024-02-02 09:10:23 +0300

    Updated MyJavaApp.java with new changes

--- src/main/java/com/example/MyJavaApp.java
+++ src/main/java/com/example/MyJavaApp.java
@@ -3,5 +3,8 @@ package com.example;
 public class MyJavaApp {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins! This is a sample Java application.");
+        System.out.println("Hello, Jenkins! This is a sample Java application.");
+
+
     }
 }
