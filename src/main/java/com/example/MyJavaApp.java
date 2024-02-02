226cfe54f88ed58407ae0cd19b7e2cce7b740f08
Dinka <naafefa123@gmail.com>
2024-02-02 09:43:23 +0300

    other commit

--- src/main/java/com/example/MyJavaApp.java
+++ src/main/java/com/example/MyJavaApp.java
@@ -4,6 +4,7 @@ public class MyJavaApp {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins! This is a sample Java application.");
         System.out.println("Hello, Jenkins! This is a sample Java application.");
+        System.out.println("Hello, Jenkins! This is a sample Java application.");
 
 
     }
