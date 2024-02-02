28aab9bfffde2a67381a59e6e2d373479e870299
Dinka <naafefa123@gmail.com>
2024-02-02 07:54:50 +0300

    initial commit

---
+++ src/main/java/com/example/MyJavaApp.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyJavaApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins! This is a sample Java application.");
+    }
+}
